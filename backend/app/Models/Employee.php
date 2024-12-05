652f656f4e53520c006b5c9e8cf11ed7572fcf3e
Joao Mattedi <joaommattedi@gmail.com>
2024-12-05 18:58:20 -0300

    feat: models fields

--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'cpf',
+        'full_name',
+        'birth_date',
+        'date_first_dose',
+        'date_second_dose',
+        'date_third_dose',
+        'vaccine_id',
+        'comorbidity_carrier',
+    ];
+
+    public function vaccine()
+    {
+        return $this->belongsTo(Vaccine::class);
+    }
 }
--- backend/app/Models/Vaccine.php
+++ backend/app/Models/Vaccine.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vaccine extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'batch',
+        'expiration_date',
+    ];
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
