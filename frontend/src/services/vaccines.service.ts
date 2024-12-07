fa2e89bbaa8a58aec096e07d8d1b98bb7fdc68ad
Joao Mattedi <joaommattedi@gmail.com>
2024-12-07 10:24:31 -0300

    feat: create vaccine request

--- frontend/src/services/vaccines.service.ts
+++ frontend/src/services/vaccines.service.ts
@@ -1,10 +1,16 @@
 import { PaginatedResponse } from "@/app/vaccines/VaccineListPage";
 import api from "./api";
+import { Vaccine } from "@/app/vaccines/new/page";
 
 export async function getVaccines(page: number) {
   const { data } = await api.get<PaginatedResponse>('/vaccines', {
     params: { page },
   });
 
+  return data;
+}
+
+export async function createVaccine(vaccine: Vaccine): Promise<Vaccine> {
+  const { data } = await api.post('/vaccines', vaccine);
   return data;
 }
